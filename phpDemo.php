d5fba1e3e04fca008afbe10f93e55b0d36ec97d4
edebar01 <36318871+edebar01@users.noreply.github.com>
2020-11-02 17:33:30 -0500

    Add files via upload

---
+++ phpDemo.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+	<h1 My First PHP Page </h1>
+	<?php
+	echo "This was written within php<br>";
+	?>
+</body>
+</html>
